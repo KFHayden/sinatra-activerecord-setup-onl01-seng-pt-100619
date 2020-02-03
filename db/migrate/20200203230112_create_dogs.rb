f9bcc7ca59160c8191b2b67f8d54327de6dffe73
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-03 23:04:15 +0000

    Automatically backed up by Learn

--- db/migrate/20200203230112_create_dogs.rb
+++ db/migrate/20200203230112_create_dogs.rb
@@ -1,6 +1,14 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
-  def change
-    create_table do |t|
-      t.string 
+  
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
   end
+  
+  def down
+    drop_table :dogs
+  end
+  
 end
